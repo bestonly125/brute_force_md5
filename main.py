7278833e4f89fdfe6e4bc5dace6edce0636a7961
jasur <best.only125@gmail.com>
2022-08-13 19:24:26 +0300

    mv to hashcat

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 from openpyxl import load_workbook
+import time
 
 # Подключаемся к обезличенному набору данных
-md5_list = open('md5_list.txt', "w")
+md5_list = open('hashcat/md5_list.txt', "w")
 wb = load_workbook("scoring_data_v.0.3.3.xlsx")
 ws = wb["A2"]
+# Oтбор соли с существующими 3 номерами телефона
+t_num = [89106881090,89057801765,89809229343]
 
 # Набор обезличенных данных преобразуем виде текстова документа для быстрого перебора
 for i in range(2,len(ws["A"])+1):
@@ -12,16 +15,17 @@ for i in range(2,len(ws["A"])+1):
 md5_list.close()
 
 # Проверка текстового документа на наличия заполнения
-read_md5_list = open("md5_list.txt", "r")
+read_md5_list = open("hashcat/md5_list.txt", "r")
 print(f"len = {bool(read_md5_list)}")
 
 # Вызываем библиотеку hashcat. Перебор данных осуществляется с помощью масок с длиной 11 символов
+start_bf = time.time()
 if bool(read_md5_list) is True:
-    os.system("./hashcat.bin -m0 -a3 md5_list.txt ?d?d?d?d?d?d?d?d?d?d?d")
-
+    os.system("rm -rf ./hashcat/hashcat.potfile")
+    os.system("./hashcat/hashcat.bin -m0 -a3 ./hashcat/md5_list.txt ?d?d?d?d?d?d?d?d?d?d?d")
+end_bf = time.time()
 # Oтбор соли с существующими 3 номерами телефона
-t_num = [89106881090,89057801765,89809229343]
-potfile = open("hashcat.potfile", "r")
+potfile = open("hashcat/hashcat.potfile", "r")
 dict_md5 = {}
 array_salts_num = []
 
@@ -36,11 +40,13 @@ for md5 in potfile:
     array_salts_num.append(int(md5[33:44]))
     dict_md5[str(md5[:32])] = md5[33:44]
 
-# На примеры 3 номеров и выявляем истинную соль
+# На примеры 3 номеров, выявляем истинную соль
+start_salt = time.time()
 salts1 = salts(t_num[0],array_salts_num)
 salts2 = salts(t_num[1],array_salts_num)
 salts3 = salts(t_num[2],array_salts_num)
 salt_res = set(salts1) & set(salts2) & set(salts3)
+end_salt = time.time()
 print(f"salt[{len(salt_res)}] = {sorted(salt_res)}")
 
 # Деобезличенный набор данных хранится в scoring_data_v.1.0.xlsx
@@ -48,5 +54,8 @@ for row in range(2,len(ws["A"])+1):
     if dict_md5[str(ws['A' + str(row)].value)]:
           ws['A' + str(row)] = int(dict_md5[str(ws['A' + str(row)].value)])-list(salt_res)[0]
 wb.save(filename="scoring_data_v.1.0.xlsx")
-os.system("rm -rf hashcat.potfile")
+print("result can see in scoring_data_v.1.0.xlsx")
+print(f"Расчетное время по brute force = {round(end_bf - start_bf,2)} сек")
+print(f"Расчетное время по соли = {round(end_salt - start_salt,2)} сек")
+os.system("libreoffice --writer scoring_data_v.1.0.xlsx")
 
